3362cf694bbfa3d0b5a4c054b7d56cd3128949fb
azizul96 <alifalamin42@gmail.com>
2023-10-26 23:24:42 +0600

    cookie

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+require('dotenv').config()
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+  }));
 app.use(express.json());
+app.use(cookieParser())
 
 
 
@@ -29,7 +36,25 @@ async function run() {
     const serviceCollection = client.db('geniusCar').collection('services')
     const bookingCollection = client.db('geniusCar').collection('bookings')
 
+    // auth api
+    app.post('/jwt', async(req, res)=>{
+      const user = req.body
+      console.log(user);
+      // const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
 
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // Set to true in production
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // Adjust based on your requirements
+        // maxAge: // how much time the cookie will exist
+    })
+      .send({success: true})
+      
+    })
+
+    // service
     app.get('/services', async(req, res)=>{
         const result = await serviceCollection.find().toArray()
         res.send(result);
@@ -46,8 +71,9 @@ async function run() {
       res.send(result)
     })
 
-    // 
+    // booking
     app.get('/bookings', async(req, res)=>{
+        // console.log('token....', req.cookies.token);
         let query = {};
         if(req.query?.email){
             query = {email: req.query.email}
